c4d63dc790896845a130ed7ce911f7d67f1b94bd
Egypratamaputra04 <37724095+Egypratamaputra04@users.noreply.github.com>
2018-11-23 20:03:22 +0700

    Add files via upload

---
+++ Perogram ScrollBarScrollPane/src/frame/data.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package frame;
+
+/**
+ *
+ * @author Ricky Hr
+ */
+
+        
+class data {
+    private String Nama;
+    private String Pekerjaan;
+    private String Hobby;
+    private String Alamat;
+    private String Email;
+    private String Tentang;
+
+    public String getNama() {
+        return Nama;
+    }
+
+    public void setNama(String nama) {
+        this.Nama = nama;
+    }
+
+    public String getPekerjaan() {
+        return Pekerjaan;
+    }
+
+    public void setPekerjaan(String pekerjaan) {
+        this.Pekerjaan = pekerjaan;
+    }
+
+    public String getHobby() {
+        return Hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.Hobby = hobby;
+    }
+
+    public String getAlamat() {
+        return Alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.Alamat = alamat;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        this.Email = email;
+    }
+
+    public String getTentang() {
+        return Tentang;
+    }
+
+    public void setTentang(String tentang) {
+        this.Tentang = tentang;
+    }
+}
